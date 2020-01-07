05766ac145603fffa18f59fa0346011d5c31a99f
Rafael Neves <rafael.neves@cognizant.com>
2020-01-07 14:29:47 -0300

    adding create panel page

--- src/js/admin_noticias.js
+++ src/js/admin_noticias.js
@@ -4,11 +4,11 @@ require('@babel/polyfill');
 console.log('---- admin_noticias webpack v1.0 ----');
 
 
-const newsUrl = 'https://api.myjson.com/bins/uo5vm';
+const newsUrl = '/Servicos/YPE.WebService.asmx/getNoticias?cargo=&novas=true';
 
 $(document).ready(function () {
     getNews('#table-news');
-    getNews('#table-filed-news');
+    getNews('#table-archived-news');
 });
 
 
@@ -18,24 +18,25 @@ function getNews(table) {
         cache: false,
         type: 'GET',
         success: function (data) {
-            let tbodyHtml = "";
-            data.data.map(function (d) {
-                tbodyHtml +=
-                    '<tr class="row-news">' +
-                    '<td class="news__name">' + d[1] + '</td>' +
-                    '<td>' +
-                    setDropdownByStatus('publicada') +
-                    '</td>' +
-                    '<td class="news___status ' + setStatusStyle("publicada") + '">Publicada</td>' +
-                    '<td>Nome da usuária</td>' +
-                    '<td>Nome da usuária</td>' +
-                    '<td>16/11/2019</td>' +
-                    '<td class="news__group">Equipe; Supervisão; Diretoria; blablablablabla</td>' +
-                    '</tr>';
+            console.log("-->> Get Noticias: ", data);
+            // let tbodyHtml = "";
+            // data.data.map(function (d) {
+            //     tbodyHtml +=
+            //         '<tr class="row-news">' +
+            //         '<td class="news__name">' + d[1] + '</td>' +
+            //         '<td>' +
+            //         setDropdownByStatus('publicada') +
+            //         '</td>' +
+            //         '<td class="news___status ' + setStatusStyle("publicada") + '">Publicada</td>' +
+            //         '<td>Nome da usuária</td>' +
+            //         '<td>Nome da usuária</td>' +
+            //         '<td>16/11/2019</td>' +
+            //         '<td class="news__group">Equipe; Supervisão; Diretoria; blablablablabla</td>' +
+            //         '</tr>';
 
-            });
+            // });
 
-            $(table + ' tbody').append(tbodyHtml);
+            // $(table + ' tbody').append(tbodyHtml);
         },
         error: function (e) {
             console.log(e);
@@ -51,7 +52,7 @@ function setStatusStyle(status) {
         case 'agendada':
             return 'scheduled'
         case 'arquivada':
-            return 'filed'
+            return 'archived'
     }
 }
 
@@ -62,7 +63,7 @@ function setDropdownByStatus(status) {
         '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">' +
         (status !== 'arquivada' ? '<li><a href="#">Ir para notícia</a></li>' : '<li><a href="#">Publicar</a></li>') +
         '<li><a href="#">Editar</a></li>' +
-        (status !== 'arquivada' ? '<li><a href="#">Arquivar</a></li>' : '<li><a href="#">Deletar</a></li>') +
+        (status !== 'arquivada' ? '<li><a href="#" data-toggle="modal" data-target="#archivedModal">Arquivar</a></li>' : '<li><a href="#">Deletar</a></li>') +
         '<li><a href="#">Detalhes</a></li>' +
         '<li><a href="#">Alterar permissão</a></li>' +
         '</ul>' +
